ab852b10a7654e72d7d8717ba022280d7dadfefb
Muhammad Sajjad <muh.sajjadsaleem@gmail.com>
2023-04-02 22:14:17 +0500

    Add signup unit testing

---
+++ app/src/main/java/com/practice/unittesting/AuthUtil.kt
@@ -0,0 +1,19 @@
+package com.practice.unittesting
+
+object AuthUtil {
+    val users = listOf("Sajjad", "Muhammad", "Abid")
+
+    fun signUp(
+        userName: String,
+        password: String,
+        repeatPassword: String
+    ): Boolean {
+        return when {
+            userName.isEmpty() || password.isEmpty() -> false
+            users.contains(userName) -> false
+            password != repeatPassword -> false
+            password.length < 2 -> false
+            else -> true
+        }
+    }
+}
---
+++ app/src/test/java/com/practice/unittesting/AuthUtilTest.kt
@@ -0,0 +1,35 @@
+package com.practice.unittesting
+
+import org.junit.Assert.*
+import org.junit.Test
+
+class AuthUtilTest {
+    @Test
+    fun `signup function return false when username or password is empty`() {
+        val userName = ""
+        val password = ""
+        val repeatPassword=""
+        assertEquals(AuthUtil.signUp(userName, password, repeatPassword),false)
+    }
+    @Test
+    fun `signup function return false when username already taken`(){
+        val userName = "Muhammad"
+        val password = "123456"
+        val repeatPassword="123456"
+        assertEquals(AuthUtil.signUp(userName, password, repeatPassword),false)
+    }
+    @Test
+    fun `signup function return false when passwords not match`(){
+        val userName = "Sajjad"
+        val password = "123457"
+        val repeatPassword="123456"
+        assertEquals(AuthUtil.signUp(userName, password, repeatPassword),false)
+    }
+    @Test
+    fun `signup function return false when password less then 2 digit`(){
+        val userName = "Saleem"
+        val password = "1"
+        val repeatPassword="1"
+        assertEquals(AuthUtil.signUp(userName, password, repeatPassword),false)
+    }
+}
